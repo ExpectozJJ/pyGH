a42a407074f75332b911669181f2ae67029b1665
Wee JunJie <32187437+ExpectozJJ@users.noreply.github.com>
2024-04-22 11:27:59 -0400

    Add files via upload

--- conformer/conformer_classification.py
+++ conformer/conformer_classification.py
@@ -1,8 +1,42 @@
 import numpy as np 
-from pyGH.GH import uGH
+from GH import uGH
 from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+def faces(simplices):
+    faceset = set()
+    for simplex in simplices:
+        numnodes = len(simplex)
+        for r in range(numnodes, 0, -1):
+            for face in combinations(simplex, r):
+                faceset.add(tuple(sorted(face)))
+    return faceset
+
+def n_faces(face_set, n):
+    return filter(lambda face: len(face)==n+1, face_set)
+
+def boundary_operator(face_set, i):
+    source_simplices = list(n_faces(face_set, i))
+    target_simplices = list(n_faces(face_set, i-1))
+    #print(source_simplices, target_simplices)
+
+    if len(target_simplices)==0:
+        S = dok_matrix((1, len(source_simplices)), dtype=np.float64)
+        S[0, 0:len(source_simplices)] = 1
+    else:
+        source_simplices_dict = {source_simplices[j]: j for j in range(len(source_simplices))}
+        target_simplices_dict = {target_simplices[i]: i for i in range(len(target_simplices))}
+
+        S = dok_matrix((len(target_simplices), len(source_simplices)), dtype=np.float64)
+        for source_simplex in source_simplices:
+            for a in range(len(source_simplex)):
+                target_simplex = source_simplex[:a]+source_simplex[(a+1):]
+                i = target_simplices_dict[target_simplex]
+                j = source_simplices_dict[source_simplex]
+                S[i, j] = -1 if a % 2==1 else 1
+    
+    return S
 
 def GHM(all_eigval, all_eigvec):
     # Input: A persistent array of eigenvalues and eigenvectors from filtration process
